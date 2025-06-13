63e16ae12cd6da7707334d2bf785b42ff40ff855
nathibiz <nutty.mbonani@gmail.com>
2025-06-14 00:19:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# nathibiz" 
---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'business-tracker-v2';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .catch(error => console.error('Cache addAll failed:', error))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request).catch(() => caches.match('/index.html')))
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
